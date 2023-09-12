88e1c7012afdb34cb23a957264a8f71325da7cd8
Jedd <jedd@autoboxmedia.com>
2023-09-12 13:55:14 -0700

    converted to preact?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -19,5 +20,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '',
-  plugins: [react()],
+  // plugins: [react()],
+  plugins: [preact()],
 })
